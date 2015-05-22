e2b59ce7145d9a6fa712c7114f4ea4d64c479540
Mathieu Henry <henry-mathieu@live.fr>
2015-05-22 12:01:59 +0200

    first commit of the tableau infernal

---
+++ tableau_infernal/src/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+
+
+
+int main(int argc, char* args[]) {
+  
+
+
+int const nombreDeLignes = 5;
+int myTab [nombreDeLignes];	
+
+myTab[0] = 12;
+myTab[1] = 5;
+myTab[2] = 42;
+myTab[3] = 1598;
+myTab[4] = 0;
+
+int myTab2 [nombreDeLignes];
+for (int i = 0; i<nombreDeLignes; i++){
+
+	if (myTab[0]<myTab[1], myTab[2], myTab[3], myTab[4], myTab[5]){
+
+		myTab2[0] = myTab[0];}
+
+	else if (myTab[1]<myTab[0], myTab[2], myTab[3], myTab[4], myTab[5]){
+
+		myTab2[0] = myTab[1];}
+
+}
+
+
+
+/*int tableauParOrdreCroissant (myTab[nombreDeLignes]);
+
+	for (int i=0; myTab[i+1]<myTab[i]; i++){
+
+		
+	}*/
+
+ 	
+
+int afficherTableau (myTab[nombreDeLignes]);
+	
+	for (int i = 0; i<nombreDeLignes; i++){
+	
+		std::cout << myTab[i] << std::endl;
+	
+	}
+
+int afficherTableau2 (myTab2[nombreDeLignes]);
+
+	for (int i = 0; i<nombreDeLignes; i++){
+
+		std::cout << myTab2[i] << std::endl;
+	}
+
+return (0);
+}
+
+/*if (myTab[4] < myTab[3]){
+ 		myTab[3]=myTab[4];
+ 		
+ 	}
+ 	if (myTab[3] < myTab[2]){
+ 		myTab[2]=myTab[3];
+ 	}*/
