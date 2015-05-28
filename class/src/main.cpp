24eba3b4c41df8885fc9d92a009b7db20182f56b
Mathieu Henry <henry-mathieu@live.fr>
2015-05-28 21:37:59 +0200

    Tableau jusqu'a 1 million

--- class/src/main.cpp
+++ class/src/main.cpp
@@ -23,8 +23,29 @@ bool askAgain = false;
 	}
 }
 
+std::vector<int> tableauMillion(1000000,0);
+void creationDuTableau(){
 
-
+	for(int i=0; i<tableauMillion.size(); i++){
+		int valeurEgaleAi = i;
+		tableauMillion[i]=valeurEgaleAi;
+		std::cout << tableauMillion[i] << std::endl;	
+	}
+}
+std::vector<int> tableauMillionAvecPair(1000000,0);
+void creationDuTableauAvecPair(){
+	for(int i=0; i<tableauMillionAvecPair.size(); i+=2){
+		int valeurEgaleAi = i;
+		tableauMillionAvecPair[i]=valeurEgaleAi;
+		std::cout << tableauMillionAvecPair[i] << std::endl;
+	}
+}
+/*int affichageDuTableau(std::vector<int> a){
+	for(int i=0; i<a.size(); i++){
+	std::cout << a[i] << std::endl;	
+	}
+	
+}*/
 
 
 
@@ -35,7 +56,9 @@ bool askAgain = false;
 
 int main(int argc, char **argv) {
 
-demanderNom(); 
+demanderNom();
+creationDuTableauAvecPair();
+/*affichageDuTableau(std::vector<int> tableauMillion);*/
 
 	return (0);
 }
